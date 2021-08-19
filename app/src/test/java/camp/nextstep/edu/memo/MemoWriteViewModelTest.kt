1ae48f13f980fbd39a4c8a4dbd5c019b6f660d82
강현석 <kekemusa37@gmail.com>
2021-08-19 23:27:04 +0900

    :white_check_mark: test: 빈 메모를 작성하고 저장 버튼을 누르면 메모가 저장되지 않습니다 테스트 함수

--- app/src/test/java/camp/nextstep/edu/memo/MemoWriteViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/memo/MemoWriteViewModelTest.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.memo.domain.entity.Memo
 import camp.nextstep.edu.memo.domain.repository.MemoRepository
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -26,6 +27,15 @@ internal class MemoWriteViewModelTest {
         verify {
             memoRepository.save(memo = Memo(memoValue))
         }
+    }
+
+    @Test
+    fun `빈 메모를 작성하고 저장 버튼을 누르면 메모가 저장되지 않습니다`() {
+        val memoValue = ""
+
+        viewModel.saveMemo(memo = memoValue)
+
+        assertEquals(viewModel.isSavedMemo.value, MemoEvent.Cancel)
     }
 
 }
