0325ea7144cc179c4a498e40d840291b3b8bc16f
강현석 <kekemusa37@gmail.com>
2021-08-19 22:02:09 +0900

    :sparkles: feat: 데이터바인딩 적용

--- app/src/main/java/camp/nextstep/edu/memo/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/memo/MainActivity.kt
@@ -2,10 +2,23 @@ package camp.nextstep.edu.memo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import camp.nextstep.edu.memo.data.MemoRepositoryImpl
+import camp.nextstep.edu.memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 }
