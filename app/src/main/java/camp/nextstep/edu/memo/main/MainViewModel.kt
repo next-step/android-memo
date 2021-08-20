79b1ea80ddbe023414783b3ad5df69f47c700f0e
강현석 <kekemusa37@gmail.com>
2021-08-20 22:34:14 +0900

    :art: review: 실제로는 MutableStateFlow가 반환되므로 _memoList.asStateFlow()를 통해 ReadOnly 구현체를 반환해주세요.

--- app/src/main/java/camp/nextstep/edu/memo/main/MainViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/memo/main/MainViewModel.kt
@@ -6,13 +6,14 @@ import camp.nextstep.edu.memo.domain.entity.Memo
 import camp.nextstep.edu.memo.domain.repository.MemoRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel(
     private val memoRepository: MemoRepository = MemoRepositoryImpl.getInstance()
 ) : ViewModel() {
 
     private val _memoList = MutableStateFlow<List<Memo>>(emptyList())
-    val memoList: StateFlow<List<Memo>> get() = _memoList
+    val memoList: StateFlow<List<Memo>> get() = _memoList.asStateFlow()
 
     fun fetchMemoList() {
         _memoList.value = memoRepository.fetch()
