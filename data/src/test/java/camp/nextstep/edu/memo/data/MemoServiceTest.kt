8a0a683d1ad2bc01cfeeff3e8ea953ca5dde1c28
강현석 <kekemusa37@gmail.com>
2021-08-22 00:42:14 +0900

    :art: review: public으로 공개된 함수들을 이용해서 MemoService에 미리 Memo를 추가해주세요.

--- data/src/test/java/camp/nextstep/edu/memo/data/MemoServiceTest.kt
+++ data/src/test/java/camp/nextstep/edu/memo/data/MemoServiceTest.kt
@@ -4,15 +4,20 @@ import camp.nextstep.edu.memo.domain.entity.Memo
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class MemoServiceTest {
 
     private lateinit var memoService: MemoService
 
+    @BeforeEach
+    fun setup() {
+        memoService = MemoService()
+    }
+
     @Test
     fun `작성한 메모를 저장합니다`() {
-        memoService = MemoService.getInstance()
         val memo = Memo("메모 작성")
 
         memoService.save(memo)
@@ -26,16 +31,14 @@ internal class MemoServiceTest {
 
     @Test
     fun `작성된 메모들을 불러옵니다`() {
-        memoService = mockk()
         val memoList = listOf(
             Memo("메모1"),
             Memo("메모2")
         )
+        memoList.forEach(memoService::save)
 
-        every {
-            memoService.fetch()
-        } returns memoList
+        val actual = memoService.fetch()
 
-        assertThat(memoService.fetch()).isEqualTo(memoList)
+        assertThat(actual).containsAll(memoList)
     }
 }
