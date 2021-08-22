9d4b985ff7dcbfecb55c6116715fa35e634ed584
namjackson <skawo32167@naver.com>
2021-08-23 03:37:11 +0900

    feat(App) : Add navigationComponent And AddFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,12 +44,18 @@ android {
 
 dependencies {
     implementation(project(":domain"))
-    
+    implementation(project(":data"))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junit5Version") // junit4 지원
