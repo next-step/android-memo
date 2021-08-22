8939584e46a9656e20a2961f88784ba30214222d
namjackson <skawo32167@naver.com>
2021-08-23 03:43:04 +0900

    feat(Domain) : Add Memo InstanceTest

--- domain/src/main/java/camp/nextstep/edu/memo/domain/entity/Memo.kt
+++ domain/src/main/java/camp/nextstep/edu/memo/domain/entity/Memo.kt
@@ -1,6 +1,12 @@
 package camp.nextstep.edu.memo.domain.entity
 
+import java.util.*
+
 data class Memo(
     val id: String,
     val value: String,
-)
+) {
+    companion object {
+        fun newInstance(memo: String) = Memo(id = UUID.randomUUID().toString(), value = memo)
+    }
+}
