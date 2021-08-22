f30e0aa51c246fc85d9c6d25be66b95e172625c2
namjackson <skawo32167@naver.com>
2021-08-22 21:02:29 +0900

    feat(Domain) : Add Memo Entity And Repository in Domain Module

--- domain/src/main/java/camp/nextstep/edu/memo/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package camp.nextstep.edu.memo.domain
-
-class MyClass {
-}
---
+++ domain/src/main/java/camp/nextstep/edu/memo/domain/entity/Memo.kt
@@ -0,0 +1,6 @@
+package camp.nextstep.edu.memo.domain.entity
+
+data class Memo(
+    val id: String,
+    val value: String,
+)
