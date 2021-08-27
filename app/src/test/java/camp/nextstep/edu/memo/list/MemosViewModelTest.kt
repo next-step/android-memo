6c828eaa422f791c3765e9cf3ff4ac8d521520a4
namjackson <skawo32167@naver.com>
2021-08-27 19:06:49 +0900

    feat(Step3) : Add MemosViewModel delete TestCase

--- app/src/test/java/camp/nextstep/edu/memo/list/MemosViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/memo/list/MemosViewModelTest.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.memo.domain.repository.MemoRepository
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -34,5 +35,22 @@ class MemosViewModelTest {
 
         // then
         assertThat(viewModel.memoList.value).isEqualTo(list)
+    }
+
+    @Test
+    fun `메모리스트를 정상적으로 삭제해야한다`() {
+        // given
+        val targetMemo = Memo("1", "cashing Memo1")
+        every { memoRepository.getMemoList() } answers { mutableListOf() }
+        val memoIdSlot = slot<String>()
+        every { memoRepository.deleteMemo(capture(memoIdSlot)) } answers { nothing }
+        viewModel = MemosViewModel(memoRepository)
+
+        // when
+        viewModel.deleteMemo(memoId = targetMemo.id)
+
+        // then
+        val actual = memoIdSlot.captured
+        assertThat(actual).isEqualTo(targetMemo.id)
     }
 }
