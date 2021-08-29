1c2270cc637d8bd5d491243c6da893483f3a95a4
malibinYun <nightmare73@naver.com>
2021-08-29 17:13:53 +0900

    [Test] EditMemoViewModel Test 작성

--- app/src/main/java/camp/nextstep/edu/memo/edit/EditMemoViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/memo/edit/EditMemoViewModel.kt
@@ -1,6 +1,11 @@
 package camp.nextstep.edu.memo.edit
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import camp.nextstep.edu.domain.Memo
 import camp.nextstep.edu.domain.MemosSource
+import camp.nextstep.edu.memo.R
+import camp.nextstep.edu.memo.utils.SingleLiveEvent
 
 /**
  * Created By Malibin
@@ -10,5 +15,27 @@ import camp.nextstep.edu.domain.MemosSource
 class EditMemoViewModel(
     private val memosRepository: MemosSource,
 ) {
+    private val _memoSaved = SingleLiveEvent<Unit>()
+    val memoSaved: LiveData<Unit> = _memoSaved
 
+    private val _toastMessage = SingleLiveEvent<Int>()
+    val toastMessage: LiveData<Int> = _toastMessage
+
+    val title = MutableLiveData("")
+    val content = MutableLiveData("")
+
+    fun saveMemo() {
+        val title = this.title.value.orEmpty()
+        if(title.isBlank()){
+            _toastMessage.value = R.string.fill_memo_title
+            return
+        }
+        val content = this.content.value.orEmpty()
+        val memo = Memo(
+            title = title,
+            content = content,
+        )
+        memosRepository.save(memo)
+        _memoSaved.call()
+    }
 }
--- app/src/test/java/camp/nextstep/edu/memo/edit/EditMemoViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/memo/edit/EditMemoViewModelTest.kt
@@ -1,9 +1,15 @@
 package camp.nextstep.edu.memo.edit
 
+import camp.nextstep.edu.domain.Memo
+import camp.nextstep.edu.domain.MemosSource
+import camp.nextstep.edu.memo.R
 import camp.nextstep.edu.memo.utils.InstantTaskExecutorExtension
-import io.mockk.mockk
-import org.junit.Before
+import camp.nextstep.edu.memo.utils.takeValue
+import com.google.common.truth.Truth.assertThat
+import io.mockk.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.RegisterExtension
 
 /**
@@ -20,22 +26,48 @@ class EditMemoViewModelTest {
     }
 
     lateinit var editMemoViewModel: EditMemoViewModel
+    lateinit var memosRepository: MemosSource
 
-    @Before
+    @BeforeEach
     fun setUp() {
-        editMemoViewModel = EditMemoViewModel(mockk())
+        memosRepository = mockk(relaxed = true)
+        editMemoViewModel = EditMemoViewModel(memosRepository)
+    }
+
+    // LiveData의 onChanged가 호출되었는 지 테스트는 어떻게 하면 좋을 지 잘 모르겠습니다.
+    // memoSaved LiveData가 업데이트 되었는지 또는 되지 않았다는 점을 테스트 하고싶은데,
+    // 이 방법 보다는 그냥 값을 넣어서 해당 업데이트된 값으로 테스트하는 게 더 나을까요?
+    @Test
+    fun `메모의 제목과 내용을 채우면 해당 내용으로 작성 완료할 수 있다`() {
+        // given
+        val memoCaptureSlot = slot<Memo>()
+        every { memosRepository.save(capture(memoCaptureSlot)) } just Runs
+
+        // when
+        editMemoViewModel.title.value = "title"
+        editMemoViewModel.content.value = "content"
+
+        editMemoViewModel.saveMemo()
+
+        // then
+        val actualMemo = memoCaptureSlot.captured
+
+        verify(exactly = 1) { memosRepository.save(any()) }
+        assertAll(
+            { assertThat(actualMemo.title).isEqualTo("title") },
+            { assertThat(actualMemo.content).isEqualTo("content") },
+            { assertThat(editMemoViewModel.memoSaved.takeValue()).isNull() },
+        )
     }
 
     @Test
-    fun ``() {
-        // given
-
-
+    fun `메모의 제목이 null 이거나 공백이면 저장할 수 없고, 에러 메시지 이벤트를 발생시킨다`() {
         // when
-
+        editMemoViewModel.content.value = "content"
+        editMemoViewModel.saveMemo()
 
         // then
-
-
+        verify(exactly = 0) { memosRepository.save(any()) }
+        assertThat(editMemoViewModel.toastMessage.takeValue()).isEqualTo(R.string.fill_memo_title)
     }
 }
